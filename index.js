ddfb7274c12ef01bfdc2f35ff6699457f83c3f3d
ilhamgusti <ilham.wibowo51@gmail.com>
2019-09-12 07:32:00 +0700

    print " ApaBole" if  number mod 3 and 5 = 0

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const apabole = number => {
 	for (i = 1; i < number; i++) {
 		if (i % 3 == 0) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				console.log('ApaBole')
+				continue
+			}
 			console.log('Apa')
 			continue
 		} else if (i % 5 == 0) {
